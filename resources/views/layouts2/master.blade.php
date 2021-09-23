f9c54c6e3d1e5f6c027edcc20298c1cc001f226c
krismandev <krismanpratama@gmail.com>
2021-09-23 11:07:47 +0700

    error load js

--- resources/views/layouts2/master.blade.php
+++ resources/views/layouts2/master.blade.php
@@ -25,15 +25,7 @@
 </head>
 
 <body>
-    <!--[if lt IE 8]>
-            <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
-        <![endif]-->
-    <!-- preloader area start -->
-    <div id="preloader">
-        <div class="loader"></div>
-    </div>
-    <!-- preloader area end -->
-    <!-- page container area start -->
+
     <div class="page-container">
         <!-- sidebar menu area start -->
         @include('layouts2.sidebar')
@@ -455,15 +447,15 @@
     <script src="{{asset('assets/js/jquery.slicknav.min.js')}}"></script>
 
     <!-- start chart js -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.2/Chart.min.js')}}"></script>
+    {{-- <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.2/Chart.min.js')}}"></script> --}}
     <!-- start highcharts js -->
-    <script src="https://code.highcharts.com/highcharts.js')}}"></script>
+    {{-- <script src="https://code.highcharts.com/highcharts.js')}}"></script> --}}
     <!-- start zingchart js -->
-    <script src="https://cdn.zingchart.com/zingchart.min.js')}}"></script>
-    <script>
+    {{-- <script src="https://cdn.zingchart.com/zingchart.min.js')}}"></script> --}}
+    {{-- <script>
     zingchart.MODULESDIR = "https://cdn.zingchart.com/modules/";
     ZC.LICENSE = ["569d52cefae586f634c54f86dc99e6a9", "ee6b7db5b51705a13dc2339db3edaf6d"];
-    </script>
+    </script> --}}
     <!-- all line chart activation -->
     <script src="{{asset('assets/js/line-chart.js')}}"></script>
     <!-- all pie chart -->
